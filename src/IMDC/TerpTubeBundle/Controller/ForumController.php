cb2db786e16a3cc9b7e0ba52f902dcb88e485e35
paul <paul@440c3cb7-eff9-40a3-83e2-33d57095c443>
2013-12-04 21:47:19 +0000

    Added ability to edit and delete forums. Deleting a forum deletes all threads and all posts hierarchically (at least I hope it does).

--- src/IMDC/TerpTubeBundle/Controller/ForumController.php
+++ src/IMDC/TerpTubeBundle/Controller/ForumController.php
@@ -12,11 +12,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
 use Doctrine\Common\Collections\ArrayCollection;
 use IMDC\TerpTubeBundle\Entity\ResourceFile;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use IMDC\TerpTubeBundle\Entity\Forum;
 use IMDC\TerpTubeBundle\Form\Type\ForumFormType;
+use IMDC\TerpTubeBundle\Form\Type\ForumFormDeleteType;
 use IMDC\TerpTubeBundle\Entity\Media;
 
 class ForumController extends Controller
@@ -73,7 +78,7 @@ class ForumController extends Controller
             	
             //$em = $this->getDoctrine()->getManager();
         	
-            $threadrepo = $em->getRepository('IMDCTerpTubeBundle:Media');
+            $mediarepo = $em->getRepository('IMDCTerpTubeBundle:Media');
             // if the media text area isn't empty, the user has selected a media
             // file to create a new thread with
             if (!$form->get('mediatextarea')->isEmpty()) {
@@ -83,7 +88,7 @@ class ForumController extends Controller
                 $logger->info('*************media id is ' . $rawmediaID);
                 /** @var $mediaFile IMDC\TerpTubeBundle\Entity\Media */
                 $mediaFile = new Media();
-                $mediaFile = $threadrepo->findOneBy(array('id' => $rawmediaID));
+                $mediaFile = $mediarepo->findOneBy(array('id' => $rawmediaID));
                 
                 // check to make sure the user owns this media file
                 if ($user->getResourceFiles()->contains($mediaFile)) {
@@ -109,6 +114,18 @@ class ForumController extends Controller
             // persist all objects to database
             $em->flush();
         
+            // creating the ACL
+            $aclProvider = $this->get('security.acl.provider');
+            $objectIdentity = ObjectIdentity::fromDomainObject($newforum);
+            $acl = $aclProvider->createAcl($objectIdentity);
+            
+            // retrieving the security identity of the currently logged-in user
+            $securityIdentity = UserSecurityIdentity::fromAccount($user);
+            
+            // grant owner access
+            $acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
+            $aclProvider->updateAcl($acl);
+            
             $this->get('session')->getFlashBag()->add(
                     'notice',
                     'Forum created successfully!'
@@ -137,6 +154,15 @@ class ForumController extends Controller
 	    $user = $this->getUser();
 	    
 	    $forum = $em->getRepository('IMDCTerpTubeBundle:Forum')->findOneBy(array('id' => $forumid));
+	    
+	    if (!$forum) {
+	        $this->get('session')->getFlashBag()->add(
+	            'notice',
+	            'A forum with this identification does not exist!'
+	        );
+	         
+	        return $this->redirect($this->generateUrl('imdc_forum_list'));
+	    }
 	    
 // 	    $forumthreads = $em->getRepository('IMDCTerpTubeBundle:Thread')->findRecentPostsForForum(4);
 // 	    $response = $this->render('IMDCTerpTubeBundle:Forum:view.html.twig',
@@ -167,4 +193,137 @@ class ForumController extends Controller
 	    
 	}
 	
+	public function editAction(Request $request, $forumid)
+	{
+	    // check if user logged in
+	    if (!$this->container->get('imdc_terptube.authentication_manager')->isAuthenticated($request))
+	    {
+	        return $this->redirect($this->generateUrl('fos_user_security_login'));
+	    }
+	    $em = $this->getDoctrine()->getManager();
+	    $user = $this->getUser();
+	     
+	    $forum = $em->getRepository('IMDCTerpTubeBundle:Forum')->findOneBy(array('id' => $forumid));
+	    
+	    $form = $this->createForm(new ForumFormType(), $forum, array(
+	        'user' => $this->getUser(),
+	    ));
+	    
+	    $form->handleRequest($request);
+	    
+	    if ($form->isValid()) {
+	         
+	        $mediarepo = $em->getRepository('IMDCTerpTubeBundle:Media');
+	        // if the media text area isn't empty, the user has selected a new media
+	        // file to use for the forum title
+	        if (!$form->get('mediatextarea')->isEmpty()) {
+	        
+	            $rawmediaID = $form->get('mediatextarea')->getData();
+	            $logger = $this->container->get('logger');
+	            $logger->info('*************media id is ' . $rawmediaID);
+	            /** @var $mediaFile IMDC\TerpTubeBundle\Entity\Media */
+	            $mediaFile = new Media();
+	            $mediaFile = $mediarepo->findOneBy(array('id' => $rawmediaID));
+	        
+	            // check to make sure the user owns this media file
+	            if ($user->getResourceFiles()->contains($mediaFile)) {
+	                $logger = $this->get('logger');
+	                $logger->info('User owns this media file');
+	                $newforum->addTitleMedia($mediaFile);
+	            }
+	        }
+	         
+	        //$newforum->setCreator($user);
+	        //$newforum->setCreationDate(new \DateTime('now'));
+	        //             $newforum->setLocked(FALSE);
+	        //             $newforum->setSticky(FALSE);
+	        $forum->setLastActivity(new \DateTime('now'));
+	         
+	        //$user->addForum($newforum);
+	        //             $user->increasePostCount(1);
+	         
+	        // request to persist message object to database
+	        $em->persist($forum);
+	        $em->persist($user);
+	         
+	        // persist all objects to database
+	        $em->flush();
+	        
+	        $this->get('session')->getFlashBag()->add(
+	            'notice',
+	            'Forum edited successfully!'
+	        );
+	        
+	        $forumid = $forum->getId();
+	        //             return $this->redirect($this->generateUrl('imdc_forum_list'));
+	        return $this->redirect($this->generateUrl('imdc_forum_view_specific', array('forumid' => $forumid)));
+        }
+	        
+        // form not valid, show the basic form
+        return $this->render('IMDCTerpTubeBundle:Forum:edit.html.twig',
+            array('form' => $form->createView(), 'forum' => $forum
+        ));
+	}
+	
+	public function deleteAction(Request $request, $forumid)
+	{
+	    // check if user logged in
+	    if (!$this->container->get('imdc_terptube.authentication_manager')->isAuthenticated($request))
+	    {
+	        return $this->redirect($this->generateUrl('fos_user_security_login'));
+	    }
+	    $em = $this->getDoctrine()->getManager();
+	    $user = $this->getUser();
+	    
+	    $forum = $em->getRepository('IMDCTerpTubeBundle:Forum')->findOneBy(array('id' => $forumid));
+
+	    // check if user is the creator of the forum
+	    if ($user !== $forum->getCreator()) {
+	        $this->get('session')->getFlashBag()->add(
+	            'error',
+	            'You are not authorized to delete this forum'
+	        );
+	        
+	        return $this->render('IMDCTerpTubeBundle:Forum:view.html.twig',
+	            array('forum' => $forum
+	            ));
+	    }
+	    
+	    $form = $this->createForm(new ForumFormDeleteType(), $forum);
+	     
+	    $form->handleRequest($request);
+	     
+	    if ($form->isValid()) {
+	    
+	        $threads = $forum->getThreads();
+	        foreach ($threads as $thread) {
+	            $threadposts = $thread->getPosts();
+	            foreach ($threadposts as $threadpost) {
+	                $threadpost->getAuthor()->removePost($threadpost);
+	                $em->remove($threadpost);
+	            }
+	            $thread->getCreator()->removeThread($thread);
+	            $em->remove($thread);
+	        }
+	        $user->removeForum($forum);
+	        $em->persist($user);
+	        $em->remove($forum);
+	    
+	        // complete all deletions and persist user object to database
+	        $em->flush();
+	         
+	        $this->get('session')->getFlashBag()->add(
+	            'notice',
+	            'Forum deleted successfully!'
+	        );
+	         
+	        $forumid = $forum->getId();
+	        return $this->redirect($this->generateUrl('imdc_forum_list'));
+	    }
+	     
+	    // form not valid, show the basic form
+	    return $this->render('IMDCTerpTubeBundle:Forum:delete.html.twig',
+	        array('form' => $form->createView(), 'forum' => $forum
+        ));
+	}
 }
---
+++ src/IMDC/TerpTubeBundle/Form/Type/ForumFormDeleteType.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace IMDC\TerpTubeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class ForumFormDeleteType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+	    
+	    $builder->add('titleText', null, array('label' => 'Text Title', 
+	                                           'disabled' => true
+	    ));
+
+		$builder->add('submit', 'submit');
+	}	
+
+	public function getName()
+	{
+		return 'ForumDeleteForm';
+	}
+	
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array('data_class' => 'IMDC\TerpTubeBundle\Entity\Forum',));
+		
+		/*
+		$resolver->setRequired(array(
+		        'user',
+        ));
+		$resolver->setAllowedTypes(array(
+		        'user' => 'IMDC\TerpTubeBundle\Entity\User',
+		));
+		*/
+	}
+}
