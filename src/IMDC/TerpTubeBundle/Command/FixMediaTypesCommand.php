3acf078deda9916173e616746beae8acb7f4506a
martin <martin@440c3cb7-eff9-40a3-83e2-33d57095c443>
2014-11-23 04:53:42 +0000

    An attempt to fix the mime type issues that are showing up on the server but work fine locally.

--- src/IMDC/TerpTubeBundle/Command/FixMediaTypesCommand.php
+++ src/IMDC/TerpTubeBundle/Command/FixMediaTypesCommand.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace IMDC\TerpTubeBundle\Command;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,73 +14,82 @@ use IMDC\TerpTubeBundle\Event\UploadEvent;
 
 class FixMediaTypesCommand extends ContainerAwareCommand
 {
-
-    protected function configure ()
-    {
-        $this->setName('imdc:media:fix-types')->setDescription('Fixes the wrongly added videos as image types');
-    }
-
-    protected function execute (InputInterface $input, OutputInterface $output)
-    {
-        $em = $this->getContainer()
-            ->get('doctrine')
-            ->getEntityManager();
-        
-        $mediaElements = $em->getRepository('IMDCTerpTubeBundle:Media')->findAll();
-        $count = 0;
-        foreach ($mediaElements as $media)
-        {
-            if ($media->getType() == Media::TYPE_IMAGE || $media->getType() == Media::TYPE_OTHER)
-            {
-                $resource = $media->getResource();
-                $resourcePath = $resource->getAbsolutePath();
-                
-                // Get mimetype
-                $finfo = finfo_open(FILEINFO_MIME_TYPE);
-                $mimeType = finfo_file($finfo, $resourcePath);
-                finfo_close($finfo);
-                
-                $output->writeln("media id: " . $media->getId() . ". Mime-type is: " . $mimeType);
-                if (! preg_match("/^video\/.*/", $mimeType))
-                    continue;
-                
-                $count ++;
-                if ($count % 20 == 0)
-                    $em->flush();
-                $type = Media::TYPE_VIDEO;
-                $output->writeln("It is a wrongfully uploaded video");
-                $media->setType($type);
-                $media->setIsReady(Media::READY_NO);
-                
-                $dispatcher = $this->getContainer()->get('event_dispatcher');
-                $dispatcher->dispatch(UploadEvent::EVENT_UPLOAD, new UploadEvent($media));
-                $em->flush();
-                // if ($media->getThumbnailPath() == NULL)
-                // {
-                // $count ++;
-                // try
-                // {
-                // $output->writeln('Generating thumbnail for media id:' . $media->getId());
-                // $thumbnailTempFile = $transcoder->createThumbnail(
-                // $media->getResource()
-                // ->getAbsolutePath(), $media->getType());
-                // $thumbnailFile = $media->getThumbnailRootDir() . "/" . $media->getResource()->getId() . ".png";
-                // $fs->rename($thumbnailTempFile, $thumbnailFile, true);
-                // $media->setThumbnailPath($media->getResource()
-                // ->getId() . ".png");
-                // }
-                // catch (IOException $e)
-                // {
-                // $output->writeln("ERROR: " . $e->getTraceAsString());
-                // }
-                // catch (ExecutionFailureException $e)
-                // {
-                // $output->writeln("ERROR: " . $e->getTraceAsString());
-                // }
-                // if ($count % 20 == 0)
-                // $em->flush();
-                // }
-            }
-        }
-    }
+	protected function configure()
+	{
+		$this->setName ( 'imdc:media:fix-types' )->setDescription ( 'Fixes the wrongly added videos as image types' );
+	}
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$em = $this->getContainer ()->get ( 'doctrine' )->getEntityManager ();
+		
+		$mediaElements = $em->getRepository ( 'IMDCTerpTubeBundle:Media' )->findAll ();
+		$count = 0;
+		foreach ( $mediaElements as $media )
+		{
+			if ($media->getType () == Media::TYPE_IMAGE || $media->getType () == Media::TYPE_OTHER)
+			{
+				$resource = $media->getResource ();
+				$resourcePath = $resource->getAbsolutePath ();
+				
+				// Get mimetype
+				$finfo = finfo_open ( FILEINFO_MIME_TYPE );
+				if ($finfo)
+				{
+					$mimeType = finfo_file ( $finfo, $resourcePath );
+					finfo_close ( $finfo );
+				}
+				
+				if ($mimetype == 'application/octet-stream')
+				{
+					$finfo = finfo_open ( FILEINFO_MIME, '/usr/share/file/magic' );
+					if ($finfo)
+					{
+						$mimeType = finfo_file ( $finfo, $resourcePath );
+						finfo_close ( $finfo );
+					}
+				}
+				
+				$output->writeln ( "media id: " . $media->getId () . ". Mime-type is: " . $mimeType );
+				if (! preg_match ( "/^video\/.*/", $mimeType ))
+					continue;
+				
+				$count ++;
+				if ($count % 20 == 0)
+					$em->flush ();
+				$type = Media::TYPE_VIDEO;
+				$output->writeln ( "It is a wrongfully uploaded video" );
+				$media->setType ( $type );
+				$media->setIsReady ( Media::READY_NO );
+				
+				$dispatcher = $this->getContainer ()->get ( 'event_dispatcher' );
+				$dispatcher->dispatch ( UploadEvent::EVENT_UPLOAD, new UploadEvent ( $media ) );
+				$em->flush ();
+				// if ($media->getThumbnailPath() == NULL)
+				// {
+				// $count ++;
+				// try
+				// {
+				// $output->writeln('Generating thumbnail for media id:' . $media->getId());
+				// $thumbnailTempFile = $transcoder->createThumbnail(
+				// $media->getResource()
+				// ->getAbsolutePath(), $media->getType());
+				// $thumbnailFile = $media->getThumbnailRootDir() . "/" . $media->getResource()->getId() . ".png";
+				// $fs->rename($thumbnailTempFile, $thumbnailFile, true);
+				// $media->setThumbnailPath($media->getResource()
+				// ->getId() . ".png");
+				// }
+				// catch (IOException $e)
+				// {
+				// $output->writeln("ERROR: " . $e->getTraceAsString());
+				// }
+				// catch (ExecutionFailureException $e)
+				// {
+				// $output->writeln("ERROR: " . $e->getTraceAsString());
+				// }
+				// if ($count % 20 == 0)
+				// $em->flush();
+				// }
+			}
+		}
+	}
 }
