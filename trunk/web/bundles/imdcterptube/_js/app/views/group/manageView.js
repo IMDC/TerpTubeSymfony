1fc2767fd5a7b2a961557ba5762f22c2ba19aa69
jedey <jedey@440c3cb7-eff9-40a3-83e2-33d57095c443>
2015-03-03 23:16:11 +0000

    bug 213. refactored group management js view to be more generic in handling user select tables

--- trunk/web/bundles/imdcterptube/_js/app/views/group/manageView.js
+++ trunk/web/bundles/imdcterptube/_js/app/views/group/manageView.js
@@ -15,8 +15,7 @@ define([
         this.$formRemove = this.$container.find('form[name^=' + ManageView.FORM_NAME_REMOVE + ']');
         this.$formAdd = this.$container.find('form[name^=' + ManageView.FORM_NAME_ADD + ']');
         this.$tabs = this.$container.find(ManageView.Binder.TABS);
-        this.$members = this.$container.find(ManageView.Binder.MEMBERS);
-        this.$community = this.$container.find(ManageView.Binder.COMMUNITY);
+        this.$tabPanes = this.$container.find(ManageView.Binder.TAB_PANES);
         this.$remove = this.$container.find(ManageView.Binder.REMOVE);
         this.$add = this.$container.find(ManageView.Binder.ADD);
 
@@ -24,17 +23,24 @@ define([
         this.$remove.on('click', this.bind__onClickRemove);
         this.$add.on('click', this.bind__onClickAdd);
 
-        var membersToggle = function (e) {
+        var removeToggle = function (e) {
             this.$remove.attr('disabled', e.$selection.length == 0);
         }.bind(this);
-        this.membersTblCmp = TableComponent.table(this.$members);
-        this.membersTblCmp.subscribe(TableComponent.Event.SELECTION_CHANGE, membersToggle);
-
-        var communityToggle = function (e) {
+        var addToggle = function (e) {
             this.$add.attr('disabled', e.$selection.length == 0);
         }.bind(this);
-        this.communityTblCmp = TableComponent.table(this.$community);
-        this.communityTblCmp.subscribe(TableComponent.Event.SELECTION_CHANGE, communityToggle);
+        var removeOrAdd = function (type) {
+            return type == 'remove' ? removeToggle : addToggle;
+        };
+
+        this.tableCmps = [];
+        this.$tabPanes.each(function (index, element) {
+            var $tabPane = $(element);
+            var toggle = removeOrAdd($tabPane.data('type'));
+            var tblCmp = TableComponent.table($tabPane);
+            tblCmp.subscribe(TableComponent.Event.SELECTION_CHANGE, toggle);
+            this.tableCmps.push(tblCmp);
+        }.bind(this));
 
         $tt._instances.push(this);
     };
@@ -42,9 +48,8 @@ define([
     ManageView.TAG = 'GroupManageView';
 
     ManageView.Binder = {
-        TABS: '.group-tabs a[data-toggle="tab"]',
-        MEMBERS: '#tabMembers',
-        COMMUNITY: '#tabCommunity',
+        TABS: 'a[data-toggle="tab"]',
+        TAB_PANES: '[id^=tab]',
         REMOVE: '.group-remove',
         ADD: '.group-add'
     };
@@ -62,35 +67,36 @@ define([
         this._getFormField(this.$formSearch, 'active_tab').val($(e.target).attr('href'));
     };
 
-    ManageView.prototype._updateUsersSelectForm = function ($users, $form) {
+    ManageView.prototype._updateUsersSelectForm = function ($form, $button) {
         var $userList = this._getFormField($form, 'users');
-        $userList.html('');
 
-        $users.each(function (index, element) {
-            var $newUser = $userList.data('prototype');
-            $newUser = $($newUser.replace(/__name__/g, index));
-            $newUser.val($(element).data('uid'));
-            $userList.append($newUser);
-        });
+        $button.button('loading');
+        $button.toggleClass('disabled');
+
+        $userList.html('');
+        $.each(this.tableCmps, function (index, element) {
+            element.getSelection().each(function (index, element) {
+                var $newUser = $userList.data('prototype');
+                $newUser = $($newUser.replace(/__name__/g, index));
+                $newUser.val($(element).data('uid'));
+                $userList.append($newUser);
+            });
+        }.bind(this));
     };
 
     ManageView.prototype._onClickRemove = function (e) {
         e.preventDefault();
 
-        this.$remove.button('loading');
-        this.$remove.toggleClass('disabled');
+        this._updateUsersSelectForm(this.$formRemove, this.$remove);
 
-        this._updateUsersSelectForm(this.membersTblCmp.getSelection(), this.$formRemove);
         this.$formRemove.submit();
     };
 
     ManageView.prototype._onClickAdd = function (e) {
         e.preventDefault();
 
-        this.$add.button('loading');
-        this.$add.toggleClass('disabled');
+        this._updateUsersSelectForm(this.$formAdd, this.$add);
 
-        this._updateUsersSelectForm(this.communityTblCmp.getSelection(), this.$formAdd);
         this.$formAdd.submit();
     };
 
