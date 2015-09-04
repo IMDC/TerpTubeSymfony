e620aa3c44d178b2259df11696debbe59eb135d7
martin <martin@440c3cb7-eff9-40a3-83e2-33d57095c443>
2015-09-04 22:26:59 +0000

    Hopefully fixed the permissions problems when recording a video and then merging

--- src/IMDC/TerpTubeBundle/Controller/MyFilesController.php
+++ src/IMDC/TerpTubeBundle/Controller/MyFilesController.php
@@ -102,16 +102,18 @@ class MyFilesController extends Controller
             }
         }
 
-        $umask = umask();
-        umask(0000);
-        chmod($video->getRealPath(), 0666);
-        chmod($audio->getRealPath(), 0666);
-        umask($umask);
+       
         //TODO revise? will the uploaded file container always be webm or wav?
         if (!$isFirefox)
             $video = $video->move('/tmp/terptube-recordings', tempnam('', 'hello_video_') . '.webm');
         $audio = $audio->move('/tmp/terptube-recordings', tempnam('', 'hello_audio_') . ($isFirefox ? '.webm' : '.wav'));
 
+        $umask = umask();
+        umask(0000);
+        chmod($video->getRealPath(), 0666);
+        chmod($audio->getRealPath(), 0666);
+        umask($umask);
+        
         if ($isInterpretation) {
             $media = new Interpretation();
             $media->setSourceStartTime($sourceStartTime);
