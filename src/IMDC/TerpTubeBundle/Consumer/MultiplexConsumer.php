96eaba6cb2282480203327469a349128a0e931df
martin <martin@440c3cb7-eff9-40a3-83e2-33d57095c443>
2015-09-04 22:48:07 +0000

    Added an exception message to further debug the merging error

--- src/IMDC/TerpTubeBundle/Consumer/MultiplexConsumer.php
+++ src/IMDC/TerpTubeBundle/Consumer/MultiplexConsumer.php
@@ -87,6 +87,7 @@ class MultiplexConsumer extends AbstractMediaConsumer
             $em->persist($this->media);
             $em->flush();
         } catch (\Exception $e) {
+            $this->logger->error($e->__toString ());
             $this->logger->error($e->getTraceAsString());
             $this->sendStatusUpdate('Error');
             return self::MSG_REJECT;
